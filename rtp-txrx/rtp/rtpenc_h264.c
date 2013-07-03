dffddafaa1eccd65431d725e44ef13b52247b2b3
Contreras, Ignacio <ignacio.contreras@i2cat.net>
2013-07-03 15:11:43 +0200

    Add naive RTP tx_send_base()

--- rtp-txrx/rtp/rtpenc_h264.c
+++ rtp-txrx/rtp/rtpenc_h264.c
@@ -1,34 +1,3 @@
-
-
-//void
-//tx_send_tile(struct tx *tx, struct video_frame *frame, int pos, struct rtp *rtp_session)
-//{
-//        struct tile *tile;
-//        int last = FALSE;
-//        uint32_t ts = 0;
-//        int fragment_offset = 0;
-//
-//        assert(!frame->fragment || tx->fec_scheme == FEC_NONE); // currently no support for FEC with fragments
-//        assert(!frame->fragment || frame->tile_count); // multiple tile are not currently supported for fragmented send
-//
-//        tile = vf_get_tile(frame, pos);
-//        ts = get_local_mediatime();
-//        if(frame->fragment &&
-//                        tx->last_frame_fragment_id == frame->frame_fragment_id) {
-//                ts = tx->last_ts;
-//        } else {
-//                tx->last_frame_fragment_id = frame->frame_fragment_id;
-//                tx->last_ts = ts;
-//        }
-//        if(!frame->fragment || frame->last_fragment)
-//                last = TRUE;
-//        if(frame->fragment)
-//                fragment_offset = vf_get_tile(frame, pos)->offset;
-//          tx_send_base(tile, rtp_session, ts, last, frame->color_spec, frame->fps, frame->interlacing, pos, fragment_offset);
-
-//        tx->buffer ++;
-//}
-
 #include "config.h"
 #include "config_unix.h"
 #include "debug.h"
@@ -41,29 +10,55 @@
 #include "tfrc.h"
 #include "rtp/rtpenc.h"
 
-void tx_init(){
+void tx_init()
+{
     buffer_id =  lrand48() & 0x3fffff;
     bitrate = 6618;
     packet_rate = 1000 * mtu * 8 / bitrate;
 }
 
 void tx_send_base(struct tile *tile, struct rtp *rtp_session,
-                uint32_t ts, int send_m,
-                codec_t color_spec, double input_fps,
-                enum interlacing_t interlacing, unsigned int substream,
-                int fragment_offset)
+                  uint32_t ts, int send_m, codec_t color_spec,
+                  double input_fps, enum interlacing_t interlacing,
+                  unsigned int substream, int fragment_offset)
 {
-        int m, data_len;
+
+    char *data = tile->data;
+    int data_len = tile->data_len;
+
+    // according to previous code
+    int headers_len = 40; //+ (sizeof(video_payload_hdr_t));
+    assert(data_len + headers_len <= mtu);
+
+    char pt = PT_VIDEO;
+    int cc = 0;
+    uint32_t csrc = NULL;
+    
+    char *extn = 0;
+    uint16_t extn_len = 0;
+    uint16_t extn_type = 0;
+
+    int err = rtp_send_data(rtp_session, ts, pt, send_m, cc, &csrc, data,
+                            data_len, extn, extn_len, extn_type);
+
+
+/*
+        int m;
+        int data_len;
         // see definition in rtp_callback.h
         video_payload_hdr_t video_hdr;
         //ldgm_video_payload_hdr_t ldgm_hdr;
-        int pt = PT_VIDEO;            /* A value specified in our packet format */
+        int pt = PT_VIDEO;            // A value specified in our packet format
         char *data;
         unsigned int pos;
-        struct timespec start, stop;
+        struct timespec start;
+        struct timespec stop;
         long delta;
         uint32_t tmp;
-        unsigned int fps, fpsd, fd, fi;
+        unsigned int fps;
+        unsigned int fpsd;
+        unsigned int fd;
+        unsigned int fi;
 //        int mult_pos[FEC_MAX_MULT];
 //        int mult_index = 0;
 //        int mult_first_sent = 0;
@@ -100,7 +95,7 @@ void tx_send_base(struct tile *tile, struct rtp *rtp_session,
         tmp |= buffer_id;
         video_hdr[0] = htonl(tmp);
 
-        /* word 6 */
+        // word 6
         tmp = interlacing << 29;
         fps = round(input_fps);
         fpsd = 1;
@@ -144,21 +139,14 @@ void tx_send_base(struct tile *tile, struct rtp *rtp_session,
 //        }
 
         do {
-//                if(tx->fec_scheme == FEC_MULT) {
-//                        pos = mult_pos[mult_index];
-//                }
-
                 int offset = pos + fragment_offset;
 
                 video_hdr[1] = htonl(offset);
-//                if(fec_is_ldgm(tx)) {
-//                        ldgm_hdr[1] = htonl(offset);
-//                }
-
 
                 data = data_to_send + pos;
                 data_len = mtu - hdrs_len;
                 data_len = (data_len / 48) * 48;
+
                 if (pos + data_len >= (unsigned int) data_to_send_len) {
                         if (send_m) {
                                 m = 1;
@@ -167,11 +155,11 @@ void tx_send_base(struct tile *tile, struct rtp *rtp_session,
                 }
                 pos += data_len;
                 GET_STARTTIME;
-                if(data_len) { /* check needed for FEC_MULT */
+                if(data_len) { // check needed for FEC_MULT
                         rtp_send_data_hdr(rtp_session, ts, pt, m, 0, 0,
                                   hdr, hdr_len,
                                   data, data_len, 0, 0, 0);
-                        if(m /*&& tx->fec_scheme != FEC_NONE*/) {
+                        if (m) { //&& tx->fec_scheme != FEC_NONE
                                 int i;
                                 for(i = 0; i < 5; ++i) {
                                         rtp_send_data_hdr(rtp_session, ts, pt, m, 0, 0,
@@ -181,13 +169,6 @@ void tx_send_base(struct tile *tile, struct rtp *rtp_session,
                         }
                 }
 
-//                if(tx->fec_scheme == FEC_MULT) {
-//                        mult_pos[mult_index] = pos;
-//                        mult_first_sent ++;
-//                        if(mult_index != 0 || mult_first_sent >= (tx->mult_count - 1))
-//                                        mult_index = (mult_index + 1) % tx->mult_count;
-//                }
-
                 do {
                         GET_STOPTIME;
                         GET_DELTA;
@@ -195,14 +176,6 @@ void tx_send_base(struct tile *tile, struct rtp *rtp_session,
                                 delta += 1000000000L;
                 } while (packet_rate - delta > 0);
 
-//                /* when trippling, we need all streams goes to end */
-//                if(tx->fec_scheme == FEC_MULT) {
-//                        pos = mult_pos[tx->mult_count - 1];
-//                }
-
         } while (pos < (unsigned int) data_to_send_len);
-
-//        if(fec_is_ldgm(tx)) {
-//               ldgm_encoder_free_buffer(tx->fec_state, data_to_send);
-//        }
+*/
 }
