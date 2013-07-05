66fd732fac61abaaf7a7444b77790fc165a8cf71
Contreras, Ignacio <ignacio.contreras@i2cat.net>
2013-07-05 15:11:49 +0200

    Fix and update rtpenc_h264 to work with standard h264 payload over RTP

--- rtp-txrx/rtp/rtpenc_h264.c
+++ rtp-txrx/rtp/rtpenc_h264.c
@@ -11,6 +11,9 @@
 #include "rtp/rtpenc_h264.h"
 
 #define MAX_NALS 1024
+#define RTP_H264_PT 96
+
+//#define DEBUG
 
 struct rtp_nal_t {
     uint8_t *data;
@@ -118,12 +121,13 @@ void tx_send_base_h264(struct tile *tile, struct rtp *rtp_session,
     struct rtp_nal_t nals[MAX_NALS];
     int nnals;
 
-
     rtp_parse_nal_units(data, data_len, nals, &nnals);
 
-    printf("[DEBUG] %d NAL units found in buffer", nnals);
+    #ifdef DEBUG
+    printf("[RTPENC][DEBUG] %d NAL units found in buffer\n", nnals);
+    #endif
 
-    char pt = 96; // h264
+    char pt = RTP_H264_PT;
     int cc = 0;
     uint32_t csrc = 0;
     
@@ -131,25 +135,162 @@ void tx_send_base_h264(struct tile *tile, struct rtp *rtp_session,
     uint16_t extn_len = 0;
     uint16_t extn_type = 0;
 
-    // according to previous code
-    int headers_len = 40; //+ (sizeof(video_payload_hdr_t));
-
     int i;
     for (i = 0; i < nnals; i++) {
         struct rtp_nal_t nal = nals[i];
 
-        int rtp_size = headers_len + nal.size;
-        if (rtp_size >= 1000) {
-            printf("[WARNING] RTP packet size exceeds the MTU size\n");
+        int fragmentation = 0;
+        int nal_max_size = mtu - 40;
+        if (nal.size > nal_max_size) {
+            #ifdef DEBUG
+            printf("[RTPENC][WARNING] RTP packet size exceeds the MTU size\n");
+            #endif
+            fragmentation = 1;
         }
+
+        char *nal_header = (char *)nal.data;
+        int nal_header_size = 1;
         
-        int err = rtp_send_data(rtp_session, ts, pt, send_m, cc, &csrc,
-                                (char *)nal.data, nal.size, extn, extn_len,
-                                extn_type);
-        if (err != 0) {
-            printf("[ERROR] There was a problem sending the RTP packet\n");
-        } else {
-            printf("[INFO] NAL sent over RTP - %d bytes\n", nal.size);
+        char *nal_payload = (char *)(nal.data + nal_header_size);
+        int nal_payload_size = nal.size - nal_header_size;
+
+        #ifdef DEBUG
+        printf("[RTPENC][DEBUG] NAL header: %d %d %d %d %d %d %d %d\n",
+                ((*nal_header) & 0x80) >> 7, ((*nal_header) & 0x40) >> 6,
+                ((*nal_header) & 0x20) >> 5, ((*nal_header) & 0x10) >> 4,
+                ((*nal_header) & 0x08) >> 3, ((*nal_header) & 0x04) >> 2,
+                ((*nal_header) & 0x02) >> 1, ((*nal_header) & 0x01));
+        #endif
+
+        const char type = (char)(*nal_header & 0x1f);
+        const char nri = (char)((*nal_header & 0x60) >> 5);
+        
+        #ifdef DEBUG
+        printf("[RTPENC][DEBUG] NAL type: %d\n", (int)type);
+        printf("[RTPENC][DEBUG] NAL NRI: %d\n", (int)nri);
+        #endif
+
+        char info_type;
+        if (type >= 1 || type <= 23) {
+            info_type = 1;
+        }
+        else {
+            info_type = type;
+        }
+
+        #ifdef DEBUG
+        switch (info_type) {
+        case 0:
+        case 1:
+            printf("[RTPENC][DEBUG] Unfragmented or reconstructed NAL type\n");
+            break;
+        default:
+            printf("[RTPENC][ERROR] Non expected NAL type %d\n", (int)info_type);
+            return;
+            break;
+        }
+        #else
+        if (info_type != 0 && info_type != 1) {
+            printf("[RTPENC][ERROR] Non expected NAL type %d\n", (int)info_type);
+        }
+        #endif
+
+        if (!fragmentation) {
+            int err = rtp_send_data_hdr(rtp_session, ts, pt, send_m, cc, &csrc,
+                                        nal_header, nal_header_size,
+                                        nal_payload, nal_payload_size, extn, extn_len,
+                                        extn_type);
+            if (err <= 0) {
+                printf("[RTPENC][ERROR] There was a problem sending the RTP packet\n");
+            } else {
+                #ifdef DEBUG
+                printf("[RTPENC][INFO] NAL sent over RTP (%d bytes)\n", nal.size);
+                #endif
+            }
+        }
+        else {
+            const char fu_indicator = 28 | (nri << 5); // new type, fragmented
+            char fu_header = type | (1 << 7); // start
+
+            char frag_header[2];
+            int frag_header_size = 2;
+
+            frag_header[0] = fu_indicator;
+            frag_header[1] = fu_header;
+
+            char *frag_payload = (char *)nal_payload;
+            int frag_payload_size = nal_max_size - frag_header_size;
+
+            int remaining_payload_size = nal_payload_size;
+
+            while (remaining_payload_size + 2 > nal_max_size) {
+                
+                int err = rtp_send_data_hdr(rtp_session, ts, pt, send_m, cc, &csrc,
+                                            frag_header, frag_header_size,
+                                            frag_payload, frag_payload_size, extn, extn_len,
+                                            extn_type);
+                if (err <= 0) {
+                    printf("[RTPENC][ERROR] There was a problem sending the RTP packet\n");
+                }
+                else {
+                    #ifdef DEBUG
+                    char frag_class;
+                    switch((frag_header[1] & 0xE0) >> 5) {
+                    case 0:
+                        frag_class = '0';
+                        break;
+                    case 2:
+                        frag_class = 'E';
+                        break;
+                    case 4:
+                        frag_class = 'S';
+                        break;
+                    default:
+                        frag_class = '!';
+                        break;
+                    }
+                    printf("[RTPENC][INFO] NAL fragment (%c) sent over RTP (%d bytes)\n", frag_class, frag_payload_size + frag_header_size);
+                    #endif
+                }
+             
+                remaining_payload_size -= frag_payload_size;
+                frag_payload += frag_payload_size;
+                
+                frag_header[1] &= ~(1 << 7); // not end, not start 
+            }
+
+            frag_header[1] |= 1 << 6; // end
+
+            int err = rtp_send_data_hdr(rtp_session, ts, pt, send_m, cc, &csrc,
+                            frag_header, frag_header_size,
+                            frag_payload, remaining_payload_size, extn, extn_len,
+                            extn_type);
+            
+            if (err <= 0) {
+                printf("[RTPENC][ERROR] There was a problem sending the RTP packet\n");
+            }
+            else {
+                #ifdef DEBUG
+                char frag_class;
+                switch((frag_header[1] & 0xE0) >> 5) {
+                case 0:
+                    frag_class = '0';
+                    break;
+                case 2:
+                    frag_class = 'E';
+                    break;
+                case 4:
+                    frag_class = 'S';
+                    break;
+                default:
+                    frag_class = '!';
+                    break;
+                }
+                printf("[RTPENC][INFO] NAL fragment (%c) sent over RTP (%d bytes)\n",
+                    frag_class, remaining_payload_size + frag_header_size);
+                #endif
+            }
+
         }
     }
 }
