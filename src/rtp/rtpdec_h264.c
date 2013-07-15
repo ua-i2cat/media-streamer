4f02b007cf47e21ab687e0b2b861036be05905d0
Contreras, Ignacio <ignacio.contreras@i2cat.net>
2013-07-15 10:43:39 +0200

    Fix rtpdec/rtpdec_h264 include conflict

--- src/rtp/rtpdec_h264.c
+++ src/rtp/rtpdec_h264.c
@@ -7,6 +7,7 @@
 #include "rtp/rtp.h"
 #include "rtp/rtp_callback.h"
 #include "rtp/pbuf.h"
+#include "rtp/rtpdec.h"
 #include "rtp/rtpdec_h264.h"
 
 static const uint8_t start_sequence[] = { 0, 0, 0, 1 };
--- src/rtp/rtpdec_h264.h
+++ src/rtp/rtpdec_h264.h
@@ -1,3 +1,1 @@
-#include "rtp/rtpdec.h"
-
 int decode_frame_h264(struct coded_data *cdata, void *rx_data);
